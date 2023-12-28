fe64c6907af356cfac0e1e7fcc4f4067f37ae1f3
unknown <barbarbara.aberzon@post.runi.ac.il>
2023-12-28 08:29:51 +0200

    damka change

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,18 +4,15 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		int x = Integer.parseInt(args[0]) ;
-		int i = 1 ;
 		
-		while ( i <= x ){
-			String damka = "* ".repeat(x) ; //print x times "*"
-			if ( i%2 == 0 ){//if the line's number is even -> than add space in the start
-				System.out.print( " " + damka );
+		for (int i = 1 ; i <= x ; i++ ){
+			for(int j = 1 ; j <= x ; j++){
+				System.out.print( " *" ) ;
 			}
-			else {
-            System.out.print( damka );
+			System.out.println( "" );
+			if ( i%2 == 1 ){//if the line's number is even -> than add space in the start
+				System.out.print( " " );
 			}
-			System.out.println( " " );
-			i++;
 		}
 	}
 }
